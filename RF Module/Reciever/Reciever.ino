26461820999374eafcaa7f3bd8f168bf4092d786
MuhammedKhamis <muhammedessam10@gmail.com>
2018-06-18 17:12:27 +0200

    project 2nd version

--- RF Module/Reciever/Reciever.ino
+++ RF Module/Reciever/Reciever.ino
@@ -1,35 +1,31 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#define led 3
-RF24 radio(9, 53); // CE, CSN
-const byte addresses[][6] = {"00001", "00002"};
-boolean buttonState = 0;
-int prev_button_state = LOW;
-int prev_led_state = LOW;
-
+#include <Servo.h>
+Servo servo;
+const int servoPin = 6; //digital pin
+RF24 radio(40, 41); // CE, CSN
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int value;
 void setup() {
   Serial.begin(9600);
-  pinMode(led, OUTPUT);
+  // servo config
+  servo.attach(servoPin);  
+  servo.write(0);
+  
   radio.begin();
-  radio.openReadingPipe(1, addresses[0]);
-  radio.setPALevel(RF24_PA_MIN);
-}
-void loop() {
-  delay(5);
+  radio.openReadingPipe(1, pipe);
   radio.startListening();
-  while (!radio.available());
-  radio.read(&buttonState, sizeof(buttonState));
-  Serial.println(buttonState);
-  if (buttonState == HIGH && prev_button_state == LOW) {
-    if(prev_led_state == HIGH){
-      prev_led_state = LOW;
-      digitalWrite(led, LOW);
-    }
-    else{
-      prev_led_state = HIGH;
-      digitalWrite(led, HIGH);
-    }
+}
+
+void loop() {
+  if ( radio.available() )
+  {
+    radio.read( value, sizeof(value));
+    Serial.print("original value = "); Serial.println(value);
+    value = map(value, 200, 500, 0, 360);//Map value 0-1023 to 0-360 (degrees)
+    //use the maapped value for the servo
+    Serial.print("angle = "); Serial.println(value);
+    servo.write(value);
   }
-  prev_button_state = buttonState;
 }
--- RF Module/Sender/Sender.ino
+++ RF Module/Sender/Sender.ino
@@ -1,22 +1,22 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#define button 7
 RF24 radio(9, 53); // CE, CSN
-const byte addresses[][6] = {"00001", "00002"};
-boolean buttonState = 0;
+const int flexPin = A0; //analog pin
+const uint64_t pipe = 0xE8E8F0F0E1LL; //RF Channel Number
+
 void setup() {
+  pinMode(flexPin,INPUT);
   Serial.begin(9600);
-  pinMode(button, INPUT);
   radio.begin();
-  radio.openWritingPipe(addresses[0]); // 00002
-  radio.setPALevel(RF24_PA_MIN);
+  radio.openWritingPipe(pipe); // 00002
 }
+
+
 void loop() {
-  delay(5);
-  buttonState = digitalRead(button);
-  radio.write(&buttonState, sizeof(buttonState));
-  Serial.println(buttonState);
+  int value = analogRead(flexPin);
+  radio.write(value, sizeof(value));
+  Serial.println(value);
 }
   
-
+  
